26a8974ebdf34a78309050616cb1235fe469f2cf
Nicolae Furtuna <nicolae.furtuna@telenav.com>
2021-01-28 16:56:59 +0200

    Implement server and mock task POST and GET handling.

--- handler/taskhandler.go
+++ handler/taskhandler.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/nicuf/file-processor-api/task"
 )
 
 type TaskHandler struct {
@@ -13,45 +16,49 @@ func NewTaskHandler(l *log.Logger) *TaskHandler {
 	return &TaskHandler{l}
 }
 
-func (taskHandler *TaskHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-
-	if req.Method == http.MethodPost {
-		taskHandler.addTask(rw, req)
-		return
-	}
-
-	if req.Method == http.MethodGet {
-		taskHandler.getTaskInfo(rw, req)
-		return
-	}
-}
-
 //Send a file to analysis that is specified via the ID
 // swagger:route POST /addtask/{uuid} addtask addTask
 // Add a task to be processed
 // responses:
-//  default: genericError
 //	200: taskStatusResponse
-func (taskHandler *TaskHandler) addTask(rw http.ResponseWriter, req *http.Request) {
-
+func (taskHandler *TaskHandler) AddTask(rw http.ResponseWriter, req *http.Request) {
+	taskHandler.log.Println("Handle POST Task.")
+	rw.Header().Add("Content-Type", "application/json")
+	status := task.Queued
+	encoder := json.NewEncoder(rw)
+	err := encoder.Encode(status)
+	if err != nil {
+		http.Error(rw, "Unable to encode response", http.StatusInternalServerError)
+	}
 }
 
 //Get information about an analysis (execution status and results)
 // swagger:route GET /task/{id} task getTaskInfo
 // Returns task info
 // responses:
-//  default: genericError
 //  200: taskInfoResponse
-func (taskHandler *TaskHandler) getTaskInfo(rw http.ResponseWriter, req *http.Request) {
-
+func (taskHandler *TaskHandler) GetTaskInfo(rw http.ResponseWriter, req *http.Request) {
+	taskHandler.log.Println("Handle Get Task.")
+	rw.Header().Add("Content-Type", "application/json")
+	task := task.Task{
+		ID:           "1",
+		CreationTime: "today",
+		FileUUID:     "123e4567-e89b-12d3-a456-426655440000",
+		Status:       task.Finished,
+		Result:       []string{"first", "second", "third"},
+	}
+	encoder := json.NewEncoder(rw)
+	err := encoder.Encode(task)
+	if err != nil {
+		http.Error(rw, "Unable to encode response", http.StatusInternalServerError)
+	}
 }
 
 //Search for the files that contain a particular UUID
 // swagger:route GET /files/{uuid} files searchFiles
 // Returns a list of files with UUID
 // responses:
-//  default: genericError
 //  200: searchFilesResponse
-func (taskHandler *TaskHandler) searchFiles(rw http.ResponseWriter, req *http.Request) {
+func (taskHandler *TaskHandler) SearchFiles(rw http.ResponseWriter, req *http.Request) {
 
 }
--- main.go
+++ main.go
@@ -1,5 +1,89 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/gorilla/mux"
+	"github.com/nicuf/file-processor-api/cache"
+	"github.com/nicuf/file-processor-api/handler"
+	"github.com/nicuf/file-processor-api/task"
+)
+
+func testCache() {
+
+	c := cache.NewRedisCache()
+	c.Set("key", task.Task{})
+
+	v, _ := c.Get("key")
+
+	fmt.Printf("%#v", v)
+
+	ch, err := c.Subscribe()
+	if err != nil {
+		fmt.Errorf("cannot subscribe")
+	}
+
+	go func(c cache.Cache) {
+		for i := 1; i <= 10; i++ {
+			t := task.Task{ID: string(i)}
+			c.Publish(&t)
+		}
+	}(c)
+
+	for msg := range ch {
+		fmt.Println(msg.Channel, msg.Payload)
+	}
+}
+
 func main() {
 
+	l := log.New(os.Stdout, "file-processor-api", log.LstdFlags)
+	h := handler.NewTaskHandler(l)
+
+	sm := mux.NewRouter()
+
+	postR := sm.Methods(http.MethodPost).Subrouter()
+	postR.HandleFunc("/addtask/{uuid}", h.AddTask)
+
+	getR := sm.Methods(http.MethodGet).Subrouter()
+	getR.HandleFunc("/task/{id}", h.GetTaskInfo)
+	getR.HandleFunc("/files/{uuid}", h.SearchFiles)
+
+	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
+	sh := middleware.Redoc(opts, nil)
+	getR.Handle("/docs", sh)
+	getR.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
+
+	s := &http.Server{}
+	s.ReadTimeout = 10 * time.Second
+	s.WriteTimeout = 10 * time.Second
+	s.IdleTimeout = 120 * time.Second
+	s.Addr = ":7070"
+	s.Handler = sm
+	s.ErrorLog = l
+
+	go func() {
+		l.Println("Starting server")
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
