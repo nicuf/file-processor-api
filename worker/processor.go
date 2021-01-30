d451b70fba673c3252350c0652ff80938651b18f
Nicolae Furtuna <nicolae.furtuna@telenav.com>
2021-01-30 22:48:51 +0200

    Remove false positives from the loop detection.

--- worker/processor.go
+++ worker/processor.go
@@ -58,26 +58,53 @@ func (p *processor) updateCache(fileUUID string, status task.TaskStatus, result 
 }
 
 func IsLoop(fileUUID string) (bool, error) {
-	readFiles := make(map[string]bool)
+	fileGraph, err := getFileGraph(fileUUID)
+	if err != nil {
+		return false, err
+	}
+	visited := make(map[string]bool)
+	return isLoop(fileGraph, visited, fileUUID), nil
+}
+
+func isLoop(fileGraph map[string][]string, visited map[string]bool, currentNode string) bool {
+	v, ok := visited[currentNode]
+	if ok && v {
+		return true
+	}
+
+	adjacentNodes, _ := fileGraph[currentNode]
+	visited[currentNode] = true
+	for _, v := range adjacentNodes {
+		result := isLoop(fileGraph, visited, v)
+		if result == true {
+			return true
+		}
+	}
+
+	visited[currentNode] = false
+	return false
+}
+
+func getFileGraph(fileUUID string) (map[string][]string, error) {
+	fileGraph := make(map[string][]string)
 	filesToRead := []string{fileUUID}
 
 	for len(filesToRead) > 0 {
 		currentFile := filesToRead[0]
 		filesToRead = filesToRead[1:]
-		if _, ok := readFiles[currentFile]; ok {
-			return true, nil
-		}
-		readFiles[currentFile] = true
-		if _, err := os.Stat(filepath.Join("input", currentFile)); err == nil {
-			uuids, err := readUUIDSFromFile(currentFile)
-			if err != nil {
-				return false, err
+		if _, ok := fileGraph[currentFile]; !ok {
+
+			if _, err := os.Stat(filepath.Join("input", currentFile)); err == nil {
+				filesFromCurrentFile, err := readUUIDSFromFile(currentFile)
+				if err != nil {
+					return nil, err
+				}
+				fileGraph[currentFile] = filesFromCurrentFile
+				filesToRead = append(filesToRead, filesFromCurrentFile...)
 			}
-			filesToRead = append(filesToRead, uuids...)
 		}
 	}
-
-	return false, nil
+	return fileGraph, nil
 }
 
 func readUUIDSFromFile(fileUUID string) ([]string, error) {
